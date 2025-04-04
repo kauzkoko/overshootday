ba4434338888ce8e2ce9a1995627e58d8542036e
kokokauz <70515421+kauzkoko@users.noreply.github.com>
2025-04-04 20:17:22 +0200

    initial commit

---
+++ app.vue
@@ -0,0 +1,29 @@
+<template>
+  <div data-tauri-drag-region class="app">test</div>
+</template>
+
+<script setup lang="ts">
+import { getCurrentWindow } from "@tauri-apps/api/window";
+const appWindow = getCurrentWindow();
+</script>
+
+<style scoped>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  background: transparent;
+  width: 100px;
+  height: 100px;
+  border: 1px solid black;
+}
+
+.app {
+  background-color: red;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100px;
+  height: 100px;
+}
+</style>
